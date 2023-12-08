e2d864905c12c972124c0a15c7b72e3e87519da9
giangvu <khachdoi@gmail.com>
2023-12-08 22:42:27 +0700

    update status controller

--- lib/Controller/KmaStatusController.php
+++ lib/Controller/KmaStatusController.php
@@ -31,7 +31,7 @@ class KmaStatusController extends Controller {
      * @NoAdminRequired
      * @NoCSRFRequired
      *
-     * @param string $status_id
+     * @param integer $status_id
      * @param string $status_name
      */
     public function createKmaStatus($status_id, $status_name) {
@@ -43,36 +43,6 @@ class KmaStatusController extends Controller {
                 ])
                 ->execute();
             return new DataResponse(['status' => 'success']);
-            
-        // $currentUser = $this->userSession->getUser();
-        // $uid = $currentUser->getUID();
-
-		// if ($this->groupManager->isAdmin($uid)) {
-        //     $work = $this->db->getQueryBuilder();
-        //     $work->select('*')
-        //         ->from('oc_kma_work')
-        //         ->where($work->expr()->eq('kma_work_id', $user->createNamedParameter($kma_work_id)));
-        //     $result = $work->execute();
-        //     $data = $result->fetch();
-        //     if ($data === false) {
-        //         return new DataResponse(["Don't have this work"], Http::STATUS_NOT_FOUND);
-        //     }
-
-        //     $query = $this->db->getQueryBuilder();
-        //     $query->insert('kma_task_in_work')
-        //         ->values([
-        //             'kma_task_id' => $query->createNamedParameter($kma_task_id),
-        //             'kma_work_id' => $query->createNamedParameter($kma_work_id),
-        //             'task_name' => $query->createNamedParameter($task_name),
-        //             'content' => $query->createNamedParameter($content),
-        //             'status' => $query->createNamedParameter($status),
-        //         ])
-        //         ->execute();
-        //     return new DataResponse(['status' => 'success']);
-        // }
-        // else {
-        //     return new DataResponse(['No admin']);
-        // }
         
     }
 
@@ -86,70 +56,8 @@ class KmaStatusController extends Controller {
             ->from('kma_status');
 
         $result = $query->execute();
-        $tasks = $result->fetchAll();
-        return ['statuses' => $tasks];
+        $statuses = $result->fetchAll();
+        return ['statuses' => $statuses];
     }
-
-    /**
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param string $kma_task_id
-     */
-    public function getKmaTaskInWork($kma_task_id) {
-        $query = $this->db->getQueryBuilder();
-        $query->select('*')
-            ->from('kma_task_in_work')
-            ->where($query->expr()->eq('kma_task_id', $query->createNamedParameter($kma_task_id)));
-
-        $result = $query->execute();
-        $data = $result->fetchAll();
-        if ($data === false) {
-            return new DataResponse([], Http::STATUS_NOT_FOUND);
-        }
-        return new DataResponse([
-            'Ma tac vu' => $data['kma_task_id'],
-            'Ma cong viec' => $data['kma_work_id'],
-            'Ten tac vu' => $data['task_name'],
-            'Noi dung' => $data['content'],
-            'Trang thai' => $data['status'],
-        ]);
-    }
-    
-    /**
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param string $kma_task_id
-     * @param string $kma_work_id
-     * @param string $task_name
-     * @param string $content
-	 * @param string $status
-     * @return JSONResponse
-     */
-    public function updateInfoTask($kma_task_id, $kma_work_id, $task_name = null, $content = null, $status = null) {
-        $query = $this->db->prepare('UPDATE `oc_kma_task_in_work` SET `task_name` = COALESCE(?, `task_name`), 
-                                                            `content` = COALESCE(?, `content`), 
-                                                            `status` = COALESCE(?, `status`), 
-                                                                WHERE `kma_task_id` = ?');
-        $query->execute(array($task_name, $content, $status, $kma_work_id, $kma_task_id));
-        return new JSONResponse(array('status' => 'success'));
-    }
-
-    /**
-     * @NoAdminRequired
-     * @NoCSRFRequired
-     *
-     * @param string $kma_task_id
-     */
-    public function deleteKmaTask($kma_task_id) {
-        $query = $this->db->getQueryBuilder();
-        $query->delete('kma_task_in_work')
-            ->where($query->expr()->eq('kma_task_id', $query->createNamedParameter($kma_task_id)))
-            ->execute();
-        return new DataResponse(['status' => 'success']);
-    }
-
-    
 
 }
